2a418feb57ebaf073b2f1f26217267aad0f702f4
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-12 16:04:39 +0200

    Impostati comandi con prompt

---
+++ action.js
@@ -0,0 +1,10 @@
+/*
+Con queste informazioni generiamo quindi la sua password personalizzata formata in questo modo:
+[nome][cognome][colore_preferito]20
+*/
+
+var user_name = prompt('What is your name?');
+var user_surname = prompt('What is you surname?');
+var user_favorite_color = prompt('What is your favorite color?');
+
+console.log("Name is " + user_name + "; Surname is " + user_surname + "; Favorite color is " + user_favorite_color);

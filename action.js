6d3a69699f1bab0b1c949fb07e0397abb38a3131
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-12 16:09:38 +0200

    Completato algoritmo con output in console

--- action.js
+++ action.js
@@ -1,10 +1,10 @@
-/*
-Con queste informazioni generiamo quindi la sua password personalizzata formata in questo modo:
-[nome][cognome][colore_preferito]20
-*/
-
 var user_name = prompt('What is your name?');
 var user_surname = prompt('What is you surname?');
 var user_favorite_color = prompt('What is your favorite color?');
+const current_year = '20';
+
+var user_password = user_name + user_surname + user_favorite_color + current_year;
 
 console.log("Name is " + user_name + "; Surname is " + user_surname + "; Favorite color is " + user_favorite_color);
+
+console.log(user_password);

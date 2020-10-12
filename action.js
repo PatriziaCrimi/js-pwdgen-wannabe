2b3c1aedaae84efeb57c7234ff39d89123019edb
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-10-12 17:52:21 +0200

    Esercizio completato con output in HTML e aggiunto stile

--- action.js
+++ action.js
@@ -1,10 +1,20 @@
+// Variables & constants initialization and assignment
 var user_name = prompt('What is your name?');
 var user_surname = prompt('What is you surname?');
 var user_favorite_color = prompt('What is your favorite color?');
 const current_year = '20';
 
+// Password generation
 var user_password = user_name + user_surname + user_favorite_color + current_year;
 
-console.log("Name is " + user_name + "; Surname is " + user_surname + "; Favorite color is " + user_favorite_color);
+// Output in Console
+console.log('The user\'s name is ' + user_name + '; his/her surname is ' + user_surname + '; his/her favorite color is ' + user_favorite_color + '.');
 
-console.log(user_password);
+console.log('The user\'s password is: ' + user_password);
+
+// Output in HTML
+document.getElementById('user-name').innerHTML = user_name;
+document.getElementById('user-surname').innerHTML = user_surname;
+document.getElementById('user-fav-color').innerHTML = user_favorite_color;
+
+document.getElementById('user-password').innerHTML = user_password;
